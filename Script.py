0224ec2f465fad2cc42b2f5f0ae41a451f54a87a
Scott Williams <ScottWilliamsSolutions@gmail.com>
2021-11-22 13:12:34 -0500

    Removing password list and blame slash

--- Script.py
+++ Script.py
@@ -2,6 +2,25 @@ import csv
 import json
 
 compromised_users = []
+slash_null_sig = ''' 
+_  _     ___   __  ____             
+/ )( \   / __) /  \(_  _)            
+) \/ (  ( (_ \(  O ) )(              
+\____/   \___/ \__/ (__)             
+ _  _   __    ___  __ _  ____  ____  
+/ )( \ / _\  / __)(  / )(  __)(    \ 
+) __ (/    \( (__  )  (  ) _)  ) D ( 
+\_)(_/\_/\_/ \___)(__\_)(____)(____/ 
+        ____  __     __   ____  _  _ 
+ ___   / ___)(  )   / _\ / ___)/ )( \
+(___)  \___ \/ (_/\/    \\___ \) __ (
+       (____/\____/\_/\_/(____/\_)(_/
+ __ _  _  _  __    __                
+(  ( \/ )( \(  )  (  )               
+/    /) \/ (/ (_/\/ (_/\             
+\_)__)\____/\____/\____/
+
+'''
 
 # Retrieving the inital file of passwords and username of compromised
 with open('passwords.csv') as password_file:
@@ -21,3 +40,7 @@ with open('boss_message.json', 'w') as boss_message:
     'message': 'Mission Success'
     }
   json.dump(boss_message_dict, boss_message)
+  
+# Removing recovered Compromised users completely
+with open('new_passwords.csv', 'w') as new_passwords_obj:
+  new_passwords_obj.write(slash_null_sig)
