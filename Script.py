7c35475244002c024949474a6f48615d8917c7cf
Scott Williams <ScottWilliamsSolutions@gmail.com>
2021-11-22 13:11:40 -0500

    Write new file and message boss

--- Script.py
+++ Script.py
@@ -1,4 +1,5 @@
 import csv
+import json
 
 compromised_users = []
 
@@ -7,3 +8,16 @@ with open('passwords.csv') as password_file:
   password_csv = csv.DictReader(password_file)
   for password_row in password_csv:
     compromised_users.append(password_row['Username'])
+
+# Writing list of compromised users as a new file
+with open('compromised_users.txt', 'w') as compromised_user_file:
+  for compromised_user in compromised_users:
+     compromised_user_file.write(compromised_user)
+
+# Messaging boss that we have the lsit of users and the mission was succesful
+with open('boss_message.json', 'w') as boss_message:
+  boss_message_dict = {
+    'recipient': 'The Boss',
+    'message': 'Mission Success'
+    }
+  json.dump(boss_message_dict, boss_message)
