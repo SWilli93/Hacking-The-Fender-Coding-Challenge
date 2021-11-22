ed11ac532958327a216f90a2ffd932a76d7a002b
Scott Williams <ScottWilliamsSolutions@gmail.com>
2021-11-22 13:09:48 -0500

    Retrieve passwords and usernames

--- Script.py
+++ Script.py
@@ -1,1 +1,9 @@
 import csv
+
+compromised_users = []
+
+# Retrieving the inital file of passwords and username of compromised
+with open('passwords.csv') as password_file:
+  password_csv = csv.DictReader(password_file)
+  for password_row in password_csv:
+    compromised_users.append(password_row['Username'])
